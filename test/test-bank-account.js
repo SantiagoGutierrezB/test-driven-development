8bdc4a1a53d3fc6c6a9a8af9f58bc9b2b39ba99f
Santiago Gutiérrez Bárcenas <sangb@live.com.mx>
2020-05-15 20:56:33 -0500

    Fixed tests

--- test/test-bank-account.js
+++ test/test-bank-account.js
@@ -45,29 +45,70 @@ describe('BankAccount', () => {
         
         it('Should get the history movements of the account', () => {
             assert.deepEqual(
-                [{transaction: 'append', amount: amount1}, 
-                {transaction: 'substract', amount: amount2}],
-                 bankAccount.history());
+                [{transaction: 'append', amount: amount1, balance: balance + amount1}, 
+                {transaction: 'substract', amount: amount2, balance: balance + amount1 - amount2}],
+                bankAccount.history());
         });
     });
     
     describe('#merge', () => {
-        let balance1 = 450750.75;
-        let balance2 = 5750.50;
-        let amount1 = 5049.25;
-        let amount2 = 1000.00;
-        let bankAccount1 = new BankAccount(balance1);
-        let bankAccount2 = new BankAccount(balance2);
-        bankAccount1.append(amount1);
-        bankAccount2.append(amount2);
+        it('Should merge bank accounts and show history of both', () => {
+            let balance1 = 450750.75;
+            let balance2 = 5750.50;
 
-        it('Should substract the amount to an account in case its positive', () => {
-            assert.deepEqual(
-                [
-                    {transaction: 'append', amount: amount1},
-                    {transaction: 'append', amount: amount2},
-                ],
-                bankAccount1.merge(bankAccount2));
+            let amount1 = 5049.25;
+            let amount2 = 1000.00;
+            
+            let bankAccount1 = new BankAccount(balance1);
+            let bankAccount2 = new BankAccount(balance2);
+        
+            bankAccount1.append(amount1);
+            bankAccount2.append(amount2);
+            
+            bankAccount1.merge(bankAccount2);
+            
+            assert.deepEqual({
+                history: [
+                    {transaction: 'append', amount: amount1, balance: balance1 + amount1},
+                    {transaction: 'append', amount: amount2, balance: balance2 + amount2},
+                ]},
+                bankAccount1.history());
+        });
+
+        it('Should add the amount of the second account to the original in case its positive', () => {
+            let balance1 = 450750.75;
+            let balance2 = 5750.50;
+
+            let amount1 = 5049.25;
+            let amount2 = 1000.00;
+            
+            let bankAccount1 = new BankAccount(balance1);
+            let bankAccount2 = new BankAccount(balance2);
+        
+            bankAccount1.append(amount1);
+            bankAccount2.append(amount2);
+            
+            bankAccount1.merge(bankAccount2);
+            
+            assert.deepEqual(bankAccount1.current(), bankAccount1.balance + bankAccount2.balance);
+        });
+        
+        it('Should substract the amount of the second account to the original in case its negative', () => {
+            let balance1 = 450750.75;
+            let balance2 = 5750.50;
+
+            let amount1 = 5049.25;
+            let amount2 = 1000.00;
+            
+            let bankAccount1 = new BankAccount(balance1);
+            let bankAccount2 = new BankAccount(balance2);
+        
+            bankAccount1.append(amount1);
+            bankAccount2.append(amount2);
+            
+            bankAccount1.merge(bankAccount2);
+            
+            assert.deepEqual(bankAccount1.current(), bankAccount1.balance - bankAccount2.balance);
         });
     });
     
